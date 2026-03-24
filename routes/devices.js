757194e64565c2fefaba6224590b803a592889c7
linmake <lin.web.05@gmail.com>
2026-03-24 07:13:22 -0700

    authentication with google

--- routes/devices.js
+++ routes/devices.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const pool = require("../db");
 
-const router = express.Router();
+const router = express.Router();     
 
 router.get("/", async (_req, res) => {
   try {
